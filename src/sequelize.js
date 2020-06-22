8d23f2f74f8d5060d5135026de98386ef096577a
Farid Esedov <esedov_fg@pulsarnpp.ru>
2020-06-22 08:21:12 +0300

    fix: sql db changed to prod

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize')
 const resource = require('./sqlConnection')
 
-const sequelize = new Sequelize(resource.dev.db, resource.dev.user, resource.dev.password, {
-    host: resource.dev.host,
+const sequelize = new Sequelize(resource.prod.db, resource.prod.user, resource.prod.password, {
+    host: resource.prod.host,
     dialect: 'mysql',
     define: {
         timestamps: false
